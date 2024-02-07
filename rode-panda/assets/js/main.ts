779125d6d7baf06c1ca13a406179b0a432d2af6c
Pandaqi <schoolofpanda@gmail.com>
2024-02-07 21:42:54 +0100

    Switch to typescript (and lazy loading where I'd forgotten)

--- rode-panda/assets/js/main.ts
+++ rode-panda/assets/js/main.ts
@@ -4,14 +4,19 @@ font1.rel = 'stylesheet';
 font1.href = 'https://fonts.googleapis.com/css2?family=Vollkorn:ital,wght@0,400;0,700;1,400;1,700&display=swap';
 
 const godefer = document.getElementsByTagName('link')[0];
-godefer.parentNode.insertBefore(font1, godefer);
+if(godefer && godefer.parentNode)
+{
+    godefer.parentNode.insertBefore(font1, godefer);
+}
 
 // Nav buttons (go to bottom, go to top, rand project)
 const gotoBottomBtn = document.getElementById('gotoBottomBtn');
 if(gotoBottomBtn)
 {
-    gotoBottomBtn.addEventListener('click', function(ev) {
-        document.getElementById('footer').scrollIntoView({
+    gotoBottomBtn.addEventListener('click', (ev) => {
+        const footer = document.getElementById('footer');
+        if(!footer) { return; }
+        footer.scrollIntoView({
             'behavior': 'smooth'
         });
         return false;
@@ -21,7 +26,7 @@ if(gotoBottomBtn)
 const gotoTopBtn = document.getElementById('gotoTopBtn')
 if(gotoTopBtn)
 {
-    gotoTopBtn.addEventListener('click', function(ev) {
+    gotoTopBtn.addEventListener('click', (ev) => {
         window.scrollTo({top: 0, behavior: 'smooth'});
         return false;
     });
@@ -30,8 +35,9 @@ if(gotoTopBtn)
 const randProjBtn = document.getElementById('randProjBtn');
 if(randProjBtn)
 {
-    randProjBtn.addEventListener('click', function(ev) {
+    randProjBtn.addEventListener('click', (ev) => {
         const metadataNode = document.getElementById("hidden-hugo-metadata");
+        if(!metadataNode || !metadataNode.dataset.pages) { return; }
         const projectList = metadataNode.dataset.pages.split(",");
 
         const randURL = projectList[Math.floor(Math.random() * projectList.length)];
